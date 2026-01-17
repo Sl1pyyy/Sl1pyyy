aae48b79680983026bd6e7ed9b21e90c3cf66063
Konlajka <k.godlewski.650@studms.ug.edu.pl>
2026-01-17 21:24:14 +0100

    z

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,7 @@
 #This is a calculator space
 print("Welcome to the calculator!")
 print('dwadadawdawwadawdawa')
-
+sdhabdhahsbfhbsahjfbashfbjhasbjfhbajhsf
 zaza
 safsafjasofassa,mnfkjasjfksajnfjnaskjnjfaskfhjeakfs
 dawdawdawd
